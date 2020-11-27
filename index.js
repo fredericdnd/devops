74209d8f0daedbe7182460d803d783e2bcdb9803
Frédéric Dinand <frederic.dinand.developer@gmail.com>
2020-11-27 14:17:51 +0100

    Change res.send

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World - Frédéric Dinand");
 });
 
 app.listen(process.env.PORT, "0.0.0.0", () => {
