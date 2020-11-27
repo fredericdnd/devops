3d18791f54162cc080ab04f22e8d7c992786c81a
Frédéric Dinand <frederic.dinand.developer@gmail.com>
2020-11-27 13:55:41 +0100

    Create index.js

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git remote add cleverapps
-https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_YOUR_APPLICATION_ID.git
+https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_e9f8f5ce-dc46-4699-bcd9-b050e76f608d.git
 git --verbose --force push cleverapps master
